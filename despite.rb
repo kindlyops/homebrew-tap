abf4d1f076ab839dc29c1b66aaa7cb9db6535b84
CircleCI <statik@users.noreply.github.com>
2016-05-31 00:34:20 +0000

    Releasing v0.0.7

--- despite.rb
+++ despite.rb
@@ -1,14 +1,19 @@
+
+
+
 class Despite < Formula
-  desc ""
+  desc "Swiss army knife for the harried operator https://www.kindlyops.com"
   homepage "https://github.com/kindlyops/despite"
-  url "https://github.com/kindlyops/despite/releases/download/v0.0.1/despite-darwin-amd64"
-  version "v0.0.1"
-  sha256 "d1c0b90735b036aaeb1c4999cfdf3634740d18c8f23b5a32ad9d6f9be408fbb3"
+  url "https://github.com/kindlyops/despite/releases/download/v0.0.7/despite-darwin-amd64"
+  version "v0.0.7"
+  sha256 "d4f8750cd62d86c549a08bfb3a2d1d360e4afa539acb4138ae42d8209521ee23
+"
 
   if OS.linux?
     if Hardware.is_64_bit?
-      url ""
-      sha256 ""
+      url "https://github.com/kindlyops/despite/releases/download/v0.0.7/despite-linux-amd64"
+      sha256 "ca4cc476d5d8a7ce8bb1f92a071d4d78c804ae36b56f01658000f516bfda4c88
+"
     end
   end
 
@@ -18,7 +23,7 @@ class Despite < Formula
     if OS.linux?
       bin.install 'despite-linux-amd64' => 'despite'
     else
-      bin.install 'despite-darwin-amd64' => 'despite' 
+      bin.install 'despite-darwin-amd64' => 'despite'
     end
   end
 
@@ -26,12 +31,12 @@ class Despite < Formula
     msg = <<-'EOM'
 Brought to you by
 
- _  ___           _ _        ___            
- | |/ (_)_ __   __| | |_   _ / _ \ _ __  ___ 
+ _  ___           _ _        ___
+ | |/ (_)_ __   __| | |_   _ / _ \ _ __  ___
  | ' /| | '_ \ / _` | | | | | | | | '_ \/ __|
  | . \| | | | | (_| | | |_| | |_| | |_) \__ \
  |_|\_\_|_| |_|\__,_|_|\__, |\___/| .__/|___/
-                         |___/      |_|        
+                         |___/      |_|
 use at your own risk.
 EOM
   end
