e14a413f9453b265a5287861d652ae5f4621acd4
CircleCI <statik@users.noreply.github.com>
2016-08-21 15:59:10 +0000

    Releasing v0.0.15

--- despite.rb
+++ despite.rb
@@ -6,14 +6,14 @@
 class Despite < Formula
   desc "Swiss army knife for the harried operator https://www.kindlyops.com"
   homepage "https://github.com/kindlyops/despite"
-  url "https://github.com/kindlyops/despite/releases/download/v0.0.14/despite-darwin-10.9-amd64"
-  version "v0.0.14"
-  sha256 "b1a8cc36079269e7474ffb3eb116b0aa77d0dfc8c0db18ff92d30588d34c2a67"
+  url "https://github.com/kindlyops/despite/releases/download/v0.0.15/despite_darwin_amd64"
+  version "v0.0.15"
+  sha256 "ae2c90a8eec004d56b51e6822edc1ee5aeea72cbb5b474a1a1de6cb7548a5b77"
 
   if OS.linux?
     if Hardware.is_64_bit?
-      url "https://github.com/kindlyops/despite/releases/download/v0.0.14/despite-linux-amd64"
-      sha256 "8e86c4a856ebeab0e36dab53e94be0e64ae720f56dde5983ea89afe84fa410b0"
+      url "https://github.com/kindlyops/despite/releases/download/v0.0.15/despite_linux_amd64"
+      sha256 "7e05fa864e6ce1d5d2a6d2e8242241cb1e1f1d7772d3017cbd495d0f45208205"
     end
   end
 
@@ -21,9 +21,9 @@ class Despite < Formula
 
   def install
     if OS.linux?
-      bin.install "despite-linux-amd64" => 'despite'
+      bin.install "despite_linux_amd64" => 'despite'
     else
-      bin.install "despite-darwin-10.9-amd64" => 'despite'
+      bin.install "despite_darwin_amd64" => 'despite'
     end
   end
 
